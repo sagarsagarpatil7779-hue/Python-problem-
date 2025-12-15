fabf21ba64609039058e282cb2bfabf63e031be4
Sagar_patil1067 <sagarsagarpatil7779@gmail.com>
2025-12-15 16:54:45 +0530

    Correct odd/even check in 010.py
    Fix logic to correctly check if a number is odd or even.

---
+++ 010.py
@@ -0,0 +1,8 @@
+#check if a number is a odd or even
+
+num= int(input ("enter a number i will check no.. is odd or even"))
+
+if 0%2:
+    print(f"{num} is even")
+else:
+    print(f"{num} is a odd")
